21834e0d26a9ce336dc554f84a4461a7edd0064f
YangSui <yangsui9999@gmail.com>
2024-04-23 23:16:43 +0800

    feature: add password generator

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
 use clap::Parser;
-use rcli::{process_csv, Opts, SubCommand};
+use rcli::{process_csv, process_genpass, Opts, SubCommand};
 fn main() -> anyhow::Result<()> {
     let opts = Opts::parse();
     match opts.cmd {
-        SubCommand::Csv(opts) => process_csv(&opts.input, &opts.output)?,
+        SubCommand::Csv(opts) => {
+            let output = if let Some(output) = opts.output {
+                output.clone()
+            } else {
+                format!("output.{}", opts.format)
+            };
+            process_csv(&opts.input, output, opts.format)?
+        }
+        SubCommand::GenPass(opts) => {
+            println!("Generate password: {:?}", opts);
+            process_genpass(
+                opts.length,
+                opts.uppercase,
+                opts.lowercase,
+                opts.numbers,
+                opts.symbol,
+            )?
+        }
     }
 
     Ok(())
--- src/opts.rs
+++ src/opts.rs
@@ -1,3 +1,5 @@
+use std::{format, str::FromStr};
+
 use anyhow::{Error, Ok};
 use clap::{command, Parser};
 
@@ -12,6 +14,15 @@ pub struct Opts {
 pub enum SubCommand {
     #[command(name = "csv", about = "convert csv file to other format")]
     Csv(CvsOpts),
+
+    #[command(name = "genpass", about = "Generate a random password")]
+    GenPass(GenPassOpts),
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum OutputFormat {
+    Json,
+    Yaml,
 }
 
 #[derive(Debug, Parser)]
@@ -19,17 +30,67 @@ pub struct CvsOpts {
     #[arg(short, long, value_parser = verify_input_file)]
     pub input: String,
     #[arg(short, long, default_value = "output.json")]
-    pub output: String,
+    pub output: Option<String>,
+    #[arg(long, value_parser = parse_format)]
+    pub format: OutputFormat,
     #[arg(short, long, default_value = ",")]
     pub delimiter: char,
     #[arg(long, default_value_t = true)]
     pub header: bool,
 }
 
+#[derive(Debug, Parser)]
+pub struct GenPassOpts {
+    #[arg(short, long, default_value_t = 16)]
+    pub length: u8,
+
+    #[arg(long, default_value_t = true)]
+    pub uppercase: bool,
+
+    #[arg(long, default_value_t = true)]
+    pub lowercase: bool,
+
+    #[arg(long, default_value_t = true)]
+    pub numbers: bool,
+
+    #[arg(long, default_value_t = true)]
+    pub symbol: bool,
+}
 fn verify_input_file(filename: &str) -> Result<String, Error> {
     if std::path::Path::new(filename).exists() {
         Ok(filename.into())
     } else {
         Err(anyhow::anyhow!("File not found"))
+    }
+}
+
+fn parse_format(format: &str) -> Result<OutputFormat, anyhow::Error> {
+    format.parse()
+}
+
+impl From<OutputFormat> for &'static str {
+    fn from(format: OutputFormat) -> Self {
+        match format {
+            OutputFormat::Json => "json",
+            OutputFormat::Yaml => "yaml",
+        }
+    }
+}
+
+impl FromStr for OutputFormat {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_lowercase().as_str() {
+            "json" => Ok(OutputFormat::Json),
+            "yaml" => Ok(OutputFormat::Yaml),
+            v => anyhow::bail!("Unsupported format:{}", v),
+        }
+    }
+}
+
+impl std::fmt::Display for OutputFormat {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", Into::<&str>::into(*self))
     }
 }
