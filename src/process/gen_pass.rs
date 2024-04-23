6a30cc18ec588aa5bde51e99921329bd245fe56b
YangSui <yangsui9999@gmail.com>
2024-04-23 23:24:25 +0800

    feature: add zxcvbn support

--- src/process/gen_pass.rs
+++ src/process/gen_pass.rs
@@ -2,6 +2,7 @@
 
 use anyhow::Ok;
 use rand::seq::SliceRandom;
+use zxcvbn::zxcvbn;
 
 const UPPER: &[u8] = b"ABCDEFGHJKLMNPQRSTUVWXYZ";
 const LOWER: &[u8] = b"abcdefghjklmnopqrstuvwxyz";
@@ -49,8 +50,10 @@ pub fn process_genpass(
     password.shuffle(&mut rng);
 
     let password = String::from_utf8(password)?;
-
     println!("{}", password);
+
+    let estimate = zxcvbn(&password, &[])?;
+    eprintln!("Password strength: {}", estimate.score());
 
     Ok(())
 }
